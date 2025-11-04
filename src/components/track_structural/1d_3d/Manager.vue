668c11c6b691caf6506e320d0d339f88b699fe32
Adrián Diaz <diaz.adrian.g@gmail.com>
2025-11-04 11:52:41 +0100

    Fix width issue

--- src/components/track_structural/1d_3d/Manager.vue
+++ src/components/track_structural/1d_3d/Manager.vue
@@ -1,15 +1,26 @@
 <script setup>
     import StructureViewer from "@/components/track_structural/1d_3d/StructureViewer.vue";
+    
+    const props = defineProps({
+        moleculeId: { type: String, required: true, default: '1CBS' }
+    });
 </script>
 
 <template>
+    <div>
+        1D left · 3D right. Click in 1D to select; Scop3P overlay is toggleable. PDB highlights auto-clamped to mapped ranges.
+    </div>
     <div class="flex-container">
         <div class="left-side flex-item">
-            LEFT
         </div>
         <div class="right-side flex-item">
-            RIGHT
-            <StructureViewer moleculeId="6MXX" width="200" bgColor="#ffffff"/>
+            <StructureViewer 
+                :moleculeId="moleculeId" 
+                hideControls="true"
+                bg-color-r="255" 
+                bg-color-g="255" 
+                bg-color-b="255"
+            />
         </div>
     </div>
 </template>
@@ -25,6 +36,7 @@
         flex-grow: 1;
         flex-shrink: 1;
         width: calc(100% / 2);
+        padding: 1em;
     }
     
     .left-side {
