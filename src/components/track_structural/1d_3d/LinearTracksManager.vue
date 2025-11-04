51ba08e6a35f0b4b385e9dd321362f748f2c20f0
Adrián Diaz <diaz.adrian.g@gmail.com>
2025-11-04 12:07:07 +0100

    Add Navigation component

---
+++ src/components/track_structural/1d_3d/LinearTracksManager.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import "@nightingale-elements/nightingale-manager";  // Import defines the custom element
+</script>
+
+<template>
+    <nightingale-manager id="mgr">
+        <table>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </nightingale-manager>
+</template>
